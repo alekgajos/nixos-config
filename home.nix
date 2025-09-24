398084e91db91f3578f44119f08460627883dc01
Aleksander Gajos <alek.gajos@gmail.com>
2025-09-24 18:58:32 +0200

    Initial commit

---
+++ home.nix
@@ -0,0 +1,13 @@
+{ lib, pkgs, ... }:
+{
+    home = {
+        packages = with pkgs; [
+            hello
+        ];
+
+        username = "alek";
+        homeDirectory = "/home/alek";
+
+        stateVersion = "23.11";
+    }
+}
