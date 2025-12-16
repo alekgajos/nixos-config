cbf4d8517553f12e28c753d6a6f575dc5a129b57
Aleksander Gajos <alek.gajos@gmail.com>
2025-12-16 17:46:14 +0100

    Add Zig tooling + Antigravity

--- flake.nix
+++ flake.nix
@@ -2,26 +2,93 @@
     description = "Home Manager configuration";
 
     inputs = {
-        nixpkgs.url = "nixpkgs/nixos-23.11";
 
-        home-manager = {
-            url = "github:nix-community/home-manager/release-23.11";
-            inputs.nixpkgs.follows = "nixpkgs";
-        };
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+
+    home-manager = {
+        url = "github:nix-community/home-manager";
+        inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+	cursor = {
+		url = "github:omarcresp/cursor-flake/main";
+		inputs.nixpkgs.follows = "nixpkgs";
+	};
+
+	quickshell = {
+		url = "github:quickshell-mirror/quickshell";
+		inputs.nixpkgs.follows = "nixpkgs";
+	};
+
+	niri = {
+		url = "github:sodiboo/niri-flake";
+		inputs.nixpkgs.follows = "nixpkgs";
+	};
+
+	dankMaterialShell = {
+		url = "github:AvengeMedia/DankMaterialShell";
+		inputs.nixpkgs.follows = "nixpkgs";
+	};
+
+    antigravity-nix = {
+      url = "github:jacopone/antigravity-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    zig-overlay = {
+      url = "github:mitchellh/zig-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    zig = {
+      url = "github:silversquirl/zig-flake/compat";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    zls = {
+      url = "github:zigtools/zls";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    neovim-nightly-overlay = {
+        url = "github:nix-community/neovim-nightly-overlay";
+    };
 
     };                                      
     
-    outputs = { nixpkgs, home-manager, ...}:
+    outputs = { nixpkgs, home-manager, ...} @ inputs:
         let
             lib = nixpkgs.lib;
             system = "x86_64-linux";
-            pkgs = import nixpkgs { inherit system; };
+            pkgs = import nixpkgs {
+                inherit system;
+                overlays = [
+                    inputs.zig-overlay.overlays.default
+                ];
+            };
         in {
             homeConfigurations = {
                 default-profile = home-manager.lib.homeManagerConfiguration {
                     inherit pkgs;
-                    modules = [./home.nix ];
+                    modules = [
+                        ./home.nix 
+                        ];
+		    extraSpecialArgs = { inherit inputs; };
                 };
             };
+
+# 
+            devShells."${system}".default = let
+                pkgs = import nixpkgs { inherit system; };
+            in pkgs.mkShell {
+
+                packages = with pkgs; [
+                    zig.packages.${system}.nightly
+                    zls.packages.${system}.zls
+                ];
+            };
+#
+
         };
 }
+
--- home.nix
+++ home.nix
@@ -1,13 +1,100 @@
-{ lib, pkgs, ... }:
+{ inputs, lib, pkgs, ... }:
 {
-    home = {
-        packages = with pkgs; [
-            hello
-        ];
 
-        username = "alek";
-        homeDirectory = "/home/alek";
+  nixpkgs.overlays = [
+    inputs.neovim-nightly-overlay.overlays.default
+  ];
 
-        stateVersion = "23.11";
-    };
+
+	imports = [
+		# inputs.niri.homeModules.niri
+		inputs.dankMaterialShell.homeModules.dankMaterialShell.default
+		# inputs.dankMaterialShell.homeModules.dankMaterialShell.niri
+	];
+	programs.dank-material-shell.enable = true;
+	# programs.niri.enable = true;
+
+	systemd.user.sessionVariables = {
+			ELECTRON_OZONE_PLATFORM_HINT="auto";
+			XDG_MENU_PREFIX="plasma-";
+			QT_QPA_PLATFORM_PLUGIN_PATH="${pkgs.libsForQt5.qt5.qtbase.bin}/lib/qt-${pkgs.libsForQt5.qt5.qtbase.version}/plugins";
+			QT_QPA_PLATFORM="wayland";
+			CODESTRAL_API_KEY="";
+		};
+
+	programs.bash = {
+		enable = true;
+		shellAliases = {
+			scala-dev = "nix develop github:typelevel/typelevel-nix#application";
+			chrome-root = "sudo -E google-chrome-stable  --no-sandbox -–ozone-platform=wayland";
+		};
+	};
+
+	home = {
+		packages = [
+			pkgs.hello
+			pkgs.kdePackages.qtmultimedia
+			inputs.cursor.packages.${pkgs.system}.default
+			pkgs.xdg-desktop-portal-gtk
+			# pkgs.xdg-desktop-portal-wlr
+			pkgs.xdg-desktop-portal-gnome
+			pkgs.xdg-desktop-portal
+			pkgs.acpi
+			pkgs.kitty
+			pkgs.python313
+			pkgs.bc
+			pkgs.lazygit
+			pkgs.smile
+			pkgs.zed-editor
+			pkgs.gnuradio
+			inputs.antigravity-nix.packages.${pkgs.system}.default
+			pkgs.zigpkgs.master
+			pkgs.neovim
+		];
+
+
+		username = "alek";
+		homeDirectory = "/home/alek";
+
+
+		stateVersion = "25.11";
+
+
+		sessionVariables = {
+			ELECTRON_OZONE_PLATFORM_HINT="auto";
+			XDG_MENU_PREFIX="plasma-";
+			QT_QPA_PLATFORM_PLUGIN_PATH="${pkgs.libsForQt5.qt5.qtbase.bin}/lib/qt-${pkgs.libsForQt5.qt5.qtbase.version}/plugins";
+			QT_QPA_PLATFORM="wayland";
+		};
+
+	};
+
+
+	xdg.enable = true;
+
+	xdg.portal = {
+		enable = true;
+		# xdgOpenUsePortal = true;
+
+		# config.niri = {
+		# 	default = ["gnome" "gtk"];
+		# 	"org.freedesktop.impl.portal.Access" = "gtk";
+		# 	"org.freedesktop.impl.portal.FileChooser" = "gtk";
+		# 	"org.freedesktop.impl.portal.ScreenCast" = "gnome";
+		# 	"org.freedesktop.impl.portal.Secret" = "gnome-keyring";
+		# };
+		#
+		extraPortals = [
+			pkgs.xdg-desktop-portal-gnome
+			pkgs.xdg-desktop-portal-gtk
+		];
+		# configPackages = [
+		# 	pkgs.xdg-desktop-portal-gnome
+		# 	pkgs.xdg-desktop-portal-gtk
+		# ];
+
+		# config.common.default = "gtk";
+	};
+
+
 }
